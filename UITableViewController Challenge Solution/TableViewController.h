f8a515076dc690437837c3aa2ea02dc547b43e5f
Shayne Stark <sstark14@gmail.com>
2015-01-16 23:06:39 -0500

    finished project

---
+++ UITableViewController Challenge Solution/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Shayne Stark on 1/16/15.
+//  Copyright (c) 2015 Spark Apps, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
